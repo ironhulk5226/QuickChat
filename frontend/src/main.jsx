0a66021455ef3a67d1156e17d2a670c34306b821
ironhulk5226 <sagareharshal5226@gmail.com>
2026-03-14 19:14:49 +0530

    minor import issues fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 import { AuthProvider } from "../context/AuthContext.jsx";
-import { ChatProvider } from "../context/chatContext.jsx";
-
+import { ChatProvider } from "../context/ChatContext.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
